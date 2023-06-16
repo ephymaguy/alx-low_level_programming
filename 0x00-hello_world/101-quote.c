612860f3d0a32e23a5c7b86861c84d80b2649ac6
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-15 20:10:07 -0700

    print to the stdput error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints to the std error
+ * Return: returns the value of 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

bacaa14fd40efcc338b50b1df8fffe9f688f2527
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-15 18:46:21 -0700

    prints using puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void){
-	puts(Programming is like building a multilingual puzzle);
-	return 0;
+/**
+ * main -  void initialization of a program that prints using puts function
+ * Return: returns a value 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

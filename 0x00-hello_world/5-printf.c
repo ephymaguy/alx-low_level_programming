c51c7b6718f56a4235f5770c9fda679e57412a38
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-15 18:54:48 -0700

    prints using printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints using the function printf
+ * Return: returns a value o
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

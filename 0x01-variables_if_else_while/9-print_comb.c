f4e771fab81dca07b9858823f9cc7599e4e50691
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-17 11:42:52 -0700

    single digits with , and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints all single numbers with , 
+ * main - prints all single numbers with ,
  * Return: returns 0 at the end of the loop
  */
 int main(void)
 {
 int i;
+
 for (i = 0; i < 10; i++)
 {
 putchar(i + '0');

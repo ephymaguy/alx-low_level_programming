19cfc57a82cf20a4045f09b2b96d255aadaff19d
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-20 10:38:36 -0700

    print  alphabets using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+
+	_putchar('\n');
+
+
+}

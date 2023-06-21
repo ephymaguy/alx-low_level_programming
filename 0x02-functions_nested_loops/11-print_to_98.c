5d3788ad4f63feca713e4694aff28f5978b297ab
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-21 13:15:34 -0700

    prints from a given num to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,11 @@ if (n <= 98)
 {
 for (; n <= 98; n++)
 {
-printf("%d, ", n);
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
 }
 printf("\n");
 }
@@ -19,8 +23,14 @@ else if (n >= 98)
 {
 for (; n >= 98; n--)
 {
-printf("%d, ", n);
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
 }
 printf("\n");
+
 }
+
 }

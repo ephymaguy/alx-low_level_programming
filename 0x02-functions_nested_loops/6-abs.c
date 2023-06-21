65deffa03d4b58236a9e08f595a43eadc52279f6
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-20 20:02:45 -0700

    absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - converts to absolute num
+ * @c: inpute of the function
+ * Return: returns c and 0
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+c = c * -1;
+}
+else
+{
+c = c * 1;
+}
+return (c);
+return (0);
+}

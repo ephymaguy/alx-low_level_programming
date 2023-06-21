77039c6389e1737b51bb2fb38c34f032f021c7b2
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-20 17:49:28 -0700

    only aplphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * _isalpha - check if it alphabets
+ * @c: the character to check.
+ * Return: returns 1 or 0
+ */
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 {	return (1);
 }
-else 
+else
 {
 	return (0);
 }

73dd5648499adb652a8982e9c1c6097282da2e15
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-23 20:35:33 -0700

    more num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,21 @@
 #include "main.h"
-
+/**
+ * more_numbers - 0 to 14
+ */
 void more_numbers(void)
 {
-	int i, x;
+int i, x;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (x = '0'; x <= '14'; x++)
-		{
-		_putchar(x);
-		}
-	_putchar('\n');
-	}
+for (i = 0; i <= 9; i++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x > 9)
+{
+_putchar((x / 10) + '0');
+}
+_putchar((x % 10) + '0');
+}
+_putchar('\n');
+}
 }

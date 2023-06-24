7fdf35883de302c6b80c78894c5dc35d582270fc
Ephraim Bello <ephraimbellos@gmail.com>
2023-06-23 20:08:52 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - print_diagonal  line
+ * @n: number of times to print
+ */
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 0; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
